4100aa53e2d795df5e023e6edd8e5bddbf334de0
Mark Servilla <mark.servilla@gmail.com>
2020-03-11 10:15:03 -0600

    Add package setup file

---
+++ src/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""":Mod: setup.py
+
+:Synopsis:
+
+:Author:
+    servilla
+
+:Created:
+    3/10/2020
+"""
+import setuptools
+
+setuptools.setup(name='pastaplus.prune',
+                 version='2020.03.10',
+                 description='Selectively remove data packages from PASTA+',
+                 author='PASTA+ project',
+                 url='https://github.com/PASTAplus/prune',
+                 license='Apache License, Version 2.0',
+                 packages=setuptools.find_packages(),
+                 include_package_data=True,
+                 exclude_package_data={
+                     '': ['settings.py, properties.py, config.py'],
+                 }, )
+
+
+def main():
+    return 0
+
+
+if __name__ == "__main__":
+    main()
