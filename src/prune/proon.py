0de610f90a172beb74edbd3bfc5adb5494a1825b
Mark Servilla <mark.servilla@gmail.com>
2020-03-14 07:13:59 -0600

    Make PEP-8 compliant

--- src/prune/proon.py
+++ src/prune/proon.py
@@ -13,34 +13,33 @@
 :Created:
     3/10/20
 """
-import click
-import daiquiri
-
 import logging
 import os
 
-from prune.config import Config
+import click
+import daiquiri
+
 from prune.package import Package
 
 cwd = os.path.dirname(os.path.realpath(__file__))
 logfile = cwd + "/prune.log"
-daiquiri.setup(level=logging.INFO,
-               outputs=(daiquiri.output.File(logfile), "stdout",))
+daiquiri.setup(level=logging.INFO, outputs=(daiquiri.output.File(logfile),
+                                            "stdout",))
 logger = daiquiri.getLogger(__name__)
 
-help_dryrun = 'Perform dry run only, do not remove any data package'
-help_doi = 'Set DOI target to tombstone'
-help_sudo = 'SUDO password on target host (if SUDO environment variable ' \
-            'is not set) '
-help_file = 'Text file with pid(s) one per line'
-help_pid = 'Package identifier targeted for pruning'
+help_dryrun = "Perform dry run only, do not remove any data package"
+help_doi = "Set DOI target to tombstone"
+help_sudo = "SUDO password on target host (if SUDO environment variable " \
+            "is not set) "
+help_file = "Text file with pid(s) one per line"
+help_pid = "Package identifier targeted for pruning"
 
-CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
+CONTEXT_SETTINGS = dict(help_option_names=["-h", "--help"])
 
 
 @click.command(context_settings=CONTEXT_SETTINGS)
 @click.argument("host", required=True)
-@click.option("--pid",  default=None, help=help_pid)
+@click.option("--pid", default=None, help=help_pid)
 @click.option("--file", default=None, help=help_file)
 @click.option("--dryrun", default=False, is_flag=True, help=help_dryrun)
 @click.option("--doi", default=False, is_flag=True, help=help_doi)
@@ -50,11 +49,11 @@ def main(host: str, pid: str, file: str, dryrun: bool, doi: bool, sudo: str):
         Prunes (/pro͞on/) data package(s) from PASTA+ repository.
 
         \b
-            HOST: PASTA+ package server targeted for package pruning.
+        HOST: PASTA+ package server targeted for package pruning.
     """
     if pid is None and file is None:
         msg = f"Usage: proon.py [OPTIONS] [PID]\n" \
-              f"Try \"proon.py -h\" for help."
+              f"Try 'proon.py -h' for help."
         print(msg)
         return 1
     else:
