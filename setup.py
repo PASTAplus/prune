702956d61f43891c31b0030c81b83e613a791592
Mark Servilla <mark.servilla@gmail.com>
2020-03-19 14:18:42 -0600

    Project configuration files

---
+++ setup.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""":Mod: setup.py
+
+:Synopsis:
+
+:Author:
+    servilla
+
+:Created:
+    3/10/2020
+"""
+from os import path
+from setuptools import find_packages, setup
+
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+with open(path.join(here, 'LICENSE'), encoding='utf-8') as f:
+    full_license = f.read()
+
+setup(
+    name="pastaplus.prune",
+    version="2020.03.19",
+    description="Selectively prune data packages from PASTA+",
+    long_description=long_description,
+    author="PASTA+ project",
+    url="https://github.com/PASTAplus/prune",
+    license=full_license,
+    packages=find_packages(where="src"),
+    include_package_data=True,
+    exclude_package_data={"": ["settings.py, properties.py, config.py"],},
+    package_dir={"": "src"},
+    python_requires=">3.6.*",
+    install_requires=["fabric>=2.5.0", "daiquiri>=2.1.1"],
+    entry_points={"console_scripts": ["prune=prune.proon:main"]},
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License',
+    ],
+)
+
+
+def main():
+    return 0
+
+
+if __name__ == "__main__":
+    main()
